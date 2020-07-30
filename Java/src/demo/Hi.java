3c8c1b6a2a3237c149842031896d3e6377d821ee
Bhagyashri-Patil10 <patilbhagyashri.patil@gmail.com>
2020-07-30 17:59:18 +0530

    hi

---
+++ Java/src/demo/Hi.java
@@ -0,0 +1,22 @@
+package demo;
+
+public class Hi {
+	
+ public static void main(String[] args) {
+	 byte a=10;
+	 System.out.println(a);
+	 short b=200;
+	 System.out.println(b);
+	 int c=300;
+	 System.out.println(c);
+	 long d=600;
+	 System.out.println(d);
+	 boolean e=true;
+	 System.out.println(e);
+	 float f=30.00f;
+	 System.out.println(f);
+	 double g=330.00;
+	 System.out.println(g);
+}
+
+}
